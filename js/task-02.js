0169243d2b0219ce6c6a0f28c1c9d0931e4714f2
Darmoroz <darmoroz99@gmail.com>
2022-10-02 19:41:02 +0300

    Done task-3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+const makeItem = (item) => {
+	return item.map((el) => {
+		const itemRef = document.createElement("li");
+		itemRef.classList.add("item");
+		itemRef.textContent = el;
+		return itemRef;
+	});
+};
+
+const refs = makeItem(ingredients);
+list.append(...refs);
+
+// const markup = ingredients
+// 	.map((ingredient) => `<li class="item">${ingredient}</li>`)
+// 	.join("");
+// list.innerHTML = markup;
